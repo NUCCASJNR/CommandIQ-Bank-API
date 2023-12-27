adcaff9c7d7bb8206d3e9f8152481f3d2ffc0f61
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-12-27 10:24:30 +0100

    Added user model

---
+++ bank/models/user.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+"""Contains user model."""
+
+from bank.models.base_model import BaseModel
+from django.contrib.auth.models import AbstractUser, Group, Permission
+
+
+def user_profile_image_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_profile_images/<user_id>/<filename>
+    return f'profile_images/{instance.id}/{filename}'
+
+
+class User(AbstractUser, BaseModel):
+    """User class"""
+
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=60, unique=True)
+    password = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    profile_picture = models.ImageField(max_length=255, null=True, upload_to=user_profile_image_path)
+
+    groups = models.ManyToManyField(Group, blank=True, related_name='bank_user_groups')
+
+    # Add related_name to resolve clashes with auth.User.user_permissions
+    user_permissions = models.ManyToManyField(Permission, blank=True, related_name='bank_user_permissions')
+
+    class Meta:
+        """Meta class"""
+        db_table = 'users'
+
+    def save(self, *args, **kwargs):
+        """
+        Save The hash password
+        :param args: Optional Arg
+        :param kwargs: Keyword argument
+        :return: user obj
+        """
+        self.set_password(self.password)
+        super(User, self).save(*args, **kwargs)
