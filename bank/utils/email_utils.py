20f1f04659a9ca3b26921a50b1098655062a6568
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-12-29 01:20:12 +0100

    Updated my utils

--- bank/utils/email_utils.py
+++ bank/utils/email_utils.py
@@ -5,9 +5,44 @@ Handles all utils relating to sending emails and also generating tokens
 """
 
 from django.conf import settings
-from django.core.mail import send_mail
-import os
+from os import getenv
 from bank.utils.redis_utils import RedisClient
+from bank.models.user import User
 
-API_KEY = os.getenv("ELASTIC_EMAIL_API_KEY")
 
+API_KEY = getenv("ELASTIC_EMAIL_API_KEY")
+
+
+class EmailUtils:
+    @staticmethod
+    def generate_verification_code():
+        """
+        Generates a random verification code
+        """
+        verification_code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=16))
+        return verification_code
+
+    @staticmethod
+    def send_verification_email(user: User):
+        """
+        Sends a verification email to the user
+        """
+        verification_code = EmailUtils.generate_verification_code()
+        user.verified = False
+        user.verification_code = verification_code
+        user.save()
+        url = "https://api.elasticemail.com/v2/email/send"
+        payload = {
+            "apikey": API_KEY,
+            "subject": "Verify your account",
+            "from": "community-catalyst@polyglotte.tech",
+            "to": user.email,
+            "bodyHtml": f"Hello {user.username},<br> Your verification code is {verification_code}",
+        }
+        redis_client = RedisClient()
+        redis_client.set(user.email, verification_code, expiry=30)
+        response = requests.post(url, data=payload)
+        if response.status_code == 200:
+            print(f'Verification code sent to {user.email}')
+        else:
+            print(f'Elastic email error: {response.text}')
--- bank/utils/redis_utils.py
+++ bank/utils/redis_utils.py
@@ -4,6 +4,7 @@
 
 import redis
 from datetime import datetime, timedelta
+import json
 
 
 class RedisClient:
@@ -14,6 +15,12 @@ class RedisClient:
     def __init__(self, host='localhost', port=6379, db=0):
         self.redis_client = redis.StrictRedis(host=host, port=port, db=db)
 
+    def status(self):
+        """
+        Returns the status of the redis server
+        """
+        return self.redis_client.ping()
+
     def set_key(self, key, value, expiry=None):
         """
         Sets a key in redis
@@ -23,9 +30,11 @@ class RedisClient:
         """
         try:
             if expiry:
-                self.redis_client.set(key, value, ex=expiry)
+                serialized_value = json.dumps(value)
+                self.redis_client.setex(key, timedelta(minutes=expiry), serialized_value)
             else:
-                self.redis_client.set(key, value)
+                serialized_value = json.dumps(value)
+                self.redis_client.set(key, serialized_value)
             return True
         except redis.RedisError as e:
             print(f"Error setting key '{key}' in Redis: {e}")
@@ -36,9 +45,14 @@ class RedisClient:
         @param key: The key to get
         """
         try:
-            return self.redis_client.get(key)
+            value_bytes = self.redis_client.get(key)
+            if value_bytes:
+                value_str = value_bytes.decode('utf-8')
+                return json.loads(value_str)
+            return None
         except redis.RedisError as e:
             print(f"Error getting key '{key}' from Redis: {e}")
+            return None
 
     def delete_key(self, key):
         """
